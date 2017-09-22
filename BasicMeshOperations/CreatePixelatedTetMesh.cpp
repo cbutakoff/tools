56b944f6dd9641da2b910f2106fd49db538a4391
Constantine Butakoff <cbutakoff@gmail.com>
2017-09-22 18:06:59 +0200

    correcting ensightwriter

--- BasicMeshOperations/CreatePixelatedTetMesh.cpp
+++ BasicMeshOperations/CreatePixelatedTetMesh.cpp
@@ -40,7 +40,40 @@ PURPOSE.  See the above copyright notice for more information.
 #include <vtkCallbackCommand.h>
 
 #include <vtkEnSightWriter.h>
+#include <vtkShortArray.h>
 
+void SaveUnstructuredGrid(vtkUnstructuredGrid *data, const char* filename) {
+
+    bool FoundCellData = false;
+
+    if (data->GetCellData()) {
+        if (data->GetCellData()->GetArray("BlockId")) {
+            FoundCellData = true;
+        }
+    }
+    if (FoundCellData == false) {
+        //Looks like we have to create it.. crap
+        vtkSmartPointer<vtkShortArray> bids = vtkSmartPointer<vtkShortArray>::New();
+        bids->SetName("BlockId");
+        for (vtkIdType i = 0; i < data->GetNumberOfCells(); i++)
+            bids->InsertNextValue(1);
+        data->GetCellData()->SetScalars(bids);
+    }
+
+    int blockids[2];
+    blockids[0] = 1;
+    blockids[1] = 0;
+    
+    vtkSmartPointer<vtkEnSightWriter> ptr_ens = vtkSmartPointer<vtkEnSightWriter>::New();
+    ptr_ens->SetNumberOfBlocks(1);
+    ptr_ens->SetBlockIDs(blockids);
+    ptr_ens->SetTimeStep(0);
+    ptr_ens->SetInputData(data);
+    ptr_ens->SetFileName(filename);
+
+    ptr_ens->Write();
+    ptr_ens->WriteCaseFile(1);
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 4) {
@@ -59,20 +92,17 @@ int main(int argc, char *argv[]) {
             vtkSmartPointer<vtkMaskFields>::New();
     vtkSmartPointer<vtkThreshold> selector =
             vtkSmartPointer<vtkThreshold>::New();
-    vtkSmartPointer<vtkEnSightWriter> writer =
-            vtkSmartPointer<vtkEnSightWriter>::New();
     vtkSmartPointer<vtkDataSetTriangleFilter> triangulator =
             vtkSmartPointer<vtkDataSetTriangleFilter>::New();
 
-    
+
 
     vtkSmartPointer<vtkCallbackCommand> progressCallback =
             CommonTools::AssociateProgressFunction(reader);
     CommonTools::AssociateProgressFunction(selector, progressCallback);
     CommonTools::AssociateProgressFunction(triangulator, progressCallback);
     CommonTools::AssociateProgressFunction(histogram, progressCallback);
-    CommonTools::AssociateProgressFunction(writer, progressCallback);
-    
+
 
 
     // Define all of the variables
@@ -91,7 +121,7 @@ int main(int argc, char *argv[]) {
 
 
     bool make_tetras = true;
-    if (strcmp(argv[5],"hexa")==0) make_tetras = false;
+    if (strcmp(argv[5], "hexa") == 0) make_tetras = false;
 
 
     // Generate cubes from labels
@@ -102,7 +132,7 @@ int main(int argc, char *argv[]) {
 
     reader->SetFileName(argv[1]);
     reader->Update();
-    
+
     histogram->SetInputConnection(reader->GetOutputPort());
     histogram->SetComponentExtent(0, endLabel, 0, 0, 0, 0);
     histogram->SetComponentOrigin(0, 0, 0);
@@ -133,7 +163,7 @@ int main(int argc, char *argv[]) {
         // select the cells for a given label
         selector->ThresholdBetween(i, i);
         selector->Update();
-        
+
         // Strip the scalars from the output
         scalarsOff->SetInputData(selector->GetOutput());
         scalarsOff->CopyAttributeOff(vtkMaskFields::POINT_DATA,
@@ -141,29 +171,26 @@ int main(int argc, char *argv[]) {
         scalarsOff->CopyAttributeOff(vtkMaskFields::CELL_DATA,
                 vtkDataSetAttributes::SCALARS);
         scalarsOff->Update();
-        
-	if(make_tetras)
-	{       
-            triangulator->SetInputConnection(scalarsOff->GetOutputPort());
-            triangulator->Update();
-        
-            writer->SetInputData(triangulator->GetOutput());
-	}
-	else
-	{       
-            writer->SetInputData(static_cast<vtkUnstructuredGrid*>(scalarsOff->GetOutput()));
-	}
 
-        
-        
+
+
+
         // output the polydata
         std::stringstream ss;
         ss << filePrefix << i; // << ".vtk";
         cout << argv[0] << " writing " << ss.str() << endl;
 
-        writer->SetBaseName(ss.str().c_str());
-        writer->Write();
-        std::cout<<"Writing finished"<<std::endl;
+
+        if (make_tetras) {
+            triangulator->SetInputConnection(scalarsOff->GetOutputPort());
+            triangulator->Update();
+
+            SaveUnstructuredGrid( triangulator->GetOutput(), ss.str().c_str() ) ;
+        } else {
+            SaveUnstructuredGrid( vtkUnstructuredGrid::SafeDownCast(scalarsOff->GetOutput()), ss.str().c_str() ) ;
+        }
+
+        std::cout << "Writing finished" << std::endl;
     }
     return EXIT_SUCCESS;
-} 
+}
