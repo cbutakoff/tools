483218a4f550022d3d32e04c0da4aad1999f55fa
Constantine Butakoff <cbutakoff@gmail.com>
2017-09-23 15:31:04 +0200

    removed copyright thingie, since this is borrowed from vtk

--- BasicMeshOperations/CreatePixelatedTetMesh.cpp
+++ BasicMeshOperations/CreatePixelatedTetMesh.cpp
@@ -1,13 +1,3 @@
-/*=========================================================================
-Copyright (c) Constantine Butakoff
-All rights reserved.
-This software is distributed WITHOUT ANY WARRANTY; without even
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the above copyright notice for more information.
-=========================================================================*/
-
-
-
 //
 // GenerateCubesFromLabels
 //   Usage: GenerateCubesFromLabels InputVolume Startlabel Endlabel
