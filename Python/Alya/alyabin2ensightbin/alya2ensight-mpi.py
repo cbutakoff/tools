16f008cc8179acd2a1456fb0caf57fc3fd2e9a55
Constantine Butakoff <cbutakoff@gmail.com>
2017-10-24 23:25:49 +0200

    corrected saving timesteps
    numpy was saving summary of the arrays

--- Python/Alya/alyabin2ensightbin/alya2ensight-mpi.py
+++ Python/Alya/alyabin2ensightbin/alya2ensight-mpi.py
@@ -703,6 +703,10 @@ if my_rank == 0:
 
 
 
+		#to make sure the whole array gets printed
+		#otherwise numpy converts to string a summary, e.g. (1,2,3,...,5,6,7)
+		np.set_printoptions(threshold=np.inf)
+
         f.write('\n')
         f.write('TIME\n')
         for timeset in timelines:
