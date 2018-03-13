d23153f416cad8f0742e6abfb08272e482ad9814
Constantine Butakoff <cbutakoff@gmail.com>
2018-03-13 16:49:52 +0100

    added progress indicator

--- BasicMeshOperations/PassScalarsReverse.cpp
+++ BasicMeshOperations/PassScalarsReverse.cpp
@@ -8,7 +8,7 @@ PURPOSE.  See the above copyright notice for more information.
 
 /*! \file
     \brief Same as PassScalars, except that the search is done for every point/cell of target, not source!
-*/
+ */
 #include "vtkPolyDataReader.h"
 #include "vtkMath.h"
 #include "vtkPointData.h"
@@ -25,231 +25,216 @@ PURPOSE.  See the above copyright notice for more information.
 #include "VTKCommonTools.h"
 #include "vtkFloatArray.h"
 
+void PassScalarsFloat(int argc, char *argv[], double radius) {
+
+    std::cout << " reading source " << argv[1] << std::endl;
+    vtkSmartPointer<vtkPolyData> sourcePd = vtkSmartPointer<vtkPolyData>::Take(
+            CommonTools::LoadShapeFromFile(argv[1]));
+
+    std::cout << " reading target " << argv[2] << std::endl;
+    vtkSmartPointer<vtkPolyData> targetPd = vtkSmartPointer<vtkPolyData>::Take(
+            CommonTools::LoadShapeFromFile(argv[2]));
 
 
-void PassScalarsFloat( int argc, char *argv[], double radius )
-{	
+    char* property_name = argv[3];
+    std::cout << " property name: " << property_name << std::endl;
 
-	std::cout << " reading source " << argv[1] << std::endl;
-	vtkSmartPointer<vtkPolyData> sourcePd = vtkSmartPointer<vtkPolyData>::Take(
-		CommonTools::LoadShapeFromFile(argv[1]) );
+    bool use_point_data = sourcePd->GetPointData()->GetArray(property_name) != NULL;
 
-	std::cout << " reading target " << argv[2] << std::endl;
-	vtkSmartPointer<vtkPolyData> targetPd = vtkSmartPointer<vtkPolyData>::Take(
-		CommonTools::LoadShapeFromFile(argv[2]) );
+    if (use_point_data) {
+        std::cout << "Copying pointdata" << std::endl;
+
+        vtkFloatArray *target_scalars = (vtkFloatArray*) targetPd -> GetPointData() -> GetArray(property_name);
+        vtkFloatArray *source_scalars = (vtkFloatArray*) sourcePd -> GetPointData() -> GetArray(property_name);
+
+        vtkSmartPointer<vtkPointLocator> in_source_locator = vtkSmartPointer<vtkPointLocator>::New();
+        in_source_locator->SetDataSet(sourcePd);
+        in_source_locator->BuildLocator();
+
+        for (int i = 0; i < targetPd -> GetNumberOfPoints(); i++) {
+            double *p1 = targetPd -> GetPoint(i);
+
+            vtkIdType pointId;
+            if (radius >= 0) {
+                double dist2;
+                pointId = in_source_locator->FindClosestPointWithinRadius(radius, p1, dist2);
+            } else
+                pointId = in_source_locator->FindClosestPoint(p1); //(p1, p2, cellId, subId, dist);
 
 
-	char* property_name = argv[3];
-	std::cout << " property name: " << property_name << std::endl;
+            if (pointId >= 0)
+                target_scalars->SetValue(i, source_scalars->GetValue(pointId));
+            //else
+            //    target_scalars->SetValue(i,-1);
+        }
+    } else {
+        std::cout << "Copying celldata" << std::endl;
 
-	bool use_point_data = sourcePd->GetPointData()->GetArray(property_name)!=NULL;
+        vtkFloatArray *target_scalars = (vtkFloatArray*) targetPd -> GetCellData() -> GetArray(property_name);
+        vtkFloatArray *source_scalars = (vtkFloatArray*) sourcePd -> GetCellData() -> GetArray(property_name);
 
-	if( use_point_data )
-	{
-		std::cout<<"Copying pointdata"<<std::endl;
+        std::cout << "Building locator" << std::endl;
+        vtkSmartPointer<vtkCellLocator> in_source_locator = vtkSmartPointer<vtkCellLocator>::New();
+        in_source_locator->SetDataSet(sourcePd);
+        in_source_locator->BuildLocator();
 
-		vtkFloatArray *target_scalars = (vtkFloatArray*) targetPd -> GetPointData() -> GetArray(property_name);
-		vtkFloatArray *source_scalars = (vtkFloatArray*) sourcePd -> GetPointData() -> GetArray(property_name);
+        for (int i = 0; i < targetPd->GetNumberOfCells(); i++) {
 
-		vtkSmartPointer<vtkPointLocator> in_source_locator = vtkSmartPointer<vtkPointLocator>::New();
-		in_source_locator->SetDataSet(sourcePd);
-		in_source_locator->BuildLocator();
+            double cell_center_p[3];
+            double pt[3];
+            double weights[3];
+            int not_used;
+            targetPd->GetCell(i)->GetParametricCenter(cell_center_p);
+            targetPd->GetCell(i)->EvaluateLocation(not_used, cell_center_p, pt, weights);
 
-		for (int i=0; i<targetPd -> GetNumberOfPoints(); i++) 
-		{
-			double *p1 = targetPd -> GetPoint(i);
-                        
-                        vtkIdType pointId;
-                        if(radius>=0)
-                        {
-                            double dist2;
-                            pointId  = in_source_locator->FindClosestPointWithinRadius(radius, p1, dist2);
-                        }
-                        else
-                            pointId  = in_source_locator->FindClosestPoint(p1);//(p1, p2, cellId, subId, dist);
+            double closestpoint[3];
+            vtkIdType cellid;
+            int subid;
+            double dist2;
 
-                        
-                        if(pointId>=0)
-                            target_scalars->SetValue(i,source_scalars->GetValue(pointId));
-                        //else
-                        //    target_scalars->SetValue(i,-1);
-		}
-	}
-	else
-	{
-		std::cout<<"Copying celldata"<<std::endl;
+            vtkIdType pointId;
+            in_source_locator->FindClosestPoint(pt, closestpoint, cellid, subid, dist2);
 
-		vtkFloatArray *target_scalars = (vtkFloatArray*) targetPd -> GetCellData() -> GetArray(property_name);
-		vtkFloatArray *source_scalars = (vtkFloatArray*) sourcePd -> GetCellData() -> GetArray(property_name);
+            if (radius >= 0)
+                if (dist2 > radius * radius)
+                    cellid = -1;
 
-                std::cout<<"Building locator"<<std::endl;
-		vtkSmartPointer<vtkCellLocator> in_source_locator = vtkSmartPointer<vtkCellLocator>::New();
-		in_source_locator->SetDataSet(sourcePd);
-		in_source_locator->BuildLocator();
+            if (cellid >= 0)
+                target_scalars->SetValue(i, source_scalars->GetValue(cellid));
+            //else
+            //    target_scalars->SetValue(i,-1);		
+        }
+    }
 
-		for( int i=0; i<targetPd->GetNumberOfCells(); i++)
-		{
-			double cell_center_p[3];
-			double pt[3];
-			double weights[3];
-			int not_used;
-			targetPd->GetCell(i)->GetParametricCenter(cell_center_p);
-			targetPd->GetCell(i)->EvaluateLocation(not_used,cell_center_p,pt,weights);
+    char outputFileName[255];
+    strcpy(outputFileName, argv[4]);
+    std::cout << " save output to " << outputFileName << std::endl;
 
-			double closestpoint[3];
-			vtkIdType cellid;
-			int subid;
-			double dist2;
-
-                        vtkIdType pointId;
-                        in_source_locator->FindClosestPoint(pt,closestpoint,cellid,subid, dist2);
-                        
-                        if(radius>=0)
-                            if(dist2>radius*radius)
-                                cellid = -1;
-
-                        if(cellid>=0)
-                            target_scalars->SetValue(i,source_scalars->GetValue(cellid));
-                        //else
-                        //    target_scalars->SetValue(i,-1);		
-                }
-	}
-
-	char outputFileName[255];
-	strcpy(outputFileName,argv[4]);
-	std::cout << " save output to " << outputFileName << std::endl;
-
-	CommonTools::SaveShapeToFile(targetPd,outputFileName);
+    CommonTools::SaveShapeToFile(targetPd, outputFileName);
 
 }
 
+void PassScalarsShort(int argc, char *argv[], double radius) {
 
 
-void PassScalarsShort( int argc, char *argv[], double radius )
-{	
+    std::cout << " reading source " << argv[1] << std::endl;
+    vtkSmartPointer<vtkPolyData> sourcePd = vtkSmartPointer<vtkPolyData>::Take(
+            CommonTools::LoadShapeFromFile(argv[1]));
+
+    std::cout << " reading target " << argv[2] << std::endl;
+    vtkSmartPointer<vtkPolyData> targetPd = vtkSmartPointer<vtkPolyData>::Take(
+            CommonTools::LoadShapeFromFile(argv[2]));
 
 
-	std::cout << " reading source " << argv[1] << std::endl;
-	vtkSmartPointer<vtkPolyData> sourcePd = vtkSmartPointer<vtkPolyData>::Take(
-		CommonTools::LoadShapeFromFile(argv[1]) );
+    char* property_name = argv[3];
+    std::cout << " property name: " << property_name << std::endl;
 
-	std::cout << " reading target " << argv[2] << std::endl;
-	vtkSmartPointer<vtkPolyData> targetPd = vtkSmartPointer<vtkPolyData>::Take(
-		CommonTools::LoadShapeFromFile(argv[2]) );
+    bool use_point_data = sourcePd->GetPointData()->GetArray(property_name) != NULL;
+
+    if (use_point_data) {
+        std::cout << "Copying pointdata" << std::endl;
+
+        vtkShortArray *target_scalars = (vtkShortArray*) targetPd -> GetPointData() -> GetArray(property_name);
+        vtkShortArray *source_scalars = (vtkShortArray*) sourcePd -> GetPointData() -> GetArray(property_name);
+
+        vtkSmartPointer<vtkPointLocator> in_source_locator = vtkSmartPointer<vtkPointLocator>::New();
+        in_source_locator->SetDataSet(sourcePd);
+        in_source_locator->BuildLocator();
+
+        for (int i = 0; i < targetPd -> GetNumberOfPoints(); i++) {
+            if(i%1000==0)
+                std::cout<<"Point "<<i<<"/"<<targetPd -> GetNumberOfPoints()<<"\r"<<std::flush;
+                
+            double *p1 = targetPd -> GetPoint(i);
+
+            vtkIdType pointId;
+            if (radius >= 0) {
+                double dist2;
+                pointId = in_source_locator->FindClosestPointWithinRadius(radius, p1, dist2);
+            } else
+                pointId = in_source_locator->FindClosestPoint(p1); //(p1, p2, cellId, subId, dist);
 
 
-	char* property_name = argv[3];
-	std::cout << " property name: " << property_name << std::endl;
+            if (pointId >= 0)
+                target_scalars->SetValue(i, source_scalars->GetValue(pointId));
+            //else
+            //    target_scalars->SetValue(i,-1);
+        }
+        std::cout<<std::endl;
 
-	bool use_point_data = sourcePd->GetPointData()->GetArray(property_name)!=NULL;
+    } else {
+        std::cout << "Copying celldata" << std::endl;
 
-	if( use_point_data )
-	{
-		std::cout<<"Copying pointdata"<<std::endl;
+        vtkShortArray *target_scalars = (vtkShortArray*) targetPd -> GetCellData() -> GetArray(property_name);
+        vtkShortArray *source_scalars = (vtkShortArray*) sourcePd -> GetCellData() -> GetArray(property_name);
 
-		vtkShortArray *target_scalars = (vtkShortArray*) targetPd -> GetPointData() -> GetArray(property_name);
-		vtkShortArray *source_scalars = (vtkShortArray*) sourcePd -> GetPointData() -> GetArray(property_name);
+        vtkSmartPointer<vtkCellLocator> in_source_locator = vtkSmartPointer<vtkCellLocator>::New();
+        in_source_locator->SetDataSet(sourcePd);
+        in_source_locator->BuildLocator();
 
-		vtkSmartPointer<vtkPointLocator> in_source_locator = vtkSmartPointer<vtkPointLocator>::New();
-		in_source_locator->SetDataSet(sourcePd);
-		in_source_locator->BuildLocator();
+        for (int i = 0; i < targetPd->GetNumberOfCells(); i++) {
+            if(i%1000==0)
+                std::cout<<"Cell "<<i<<"/"<<targetPd -> GetNumberOfCells()<<"\r"<<std::flush;
 
-		for (int i=0; i<targetPd -> GetNumberOfPoints(); i++) 
-		{
-			double *p1 = targetPd -> GetPoint(i);
-                        
-                        vtkIdType pointId;
-                        if(radius>=0)
-                        {
-                            double dist2;
-                            pointId  = in_source_locator->FindClosestPointWithinRadius(radius, p1, dist2);
-                        }
-                        else
-                            pointId  = in_source_locator->FindClosestPoint(p1);//(p1, p2, cellId, subId, dist);
+            double cell_center_p[3];
+            double pt[3];
+            double weights[3];
+            int not_used;
+            targetPd->GetCell(i)->GetParametricCenter(cell_center_p);
+            targetPd->GetCell(i)->EvaluateLocation(not_used, cell_center_p, pt, weights);
 
-                        
-                        if(pointId>=0)
-                            target_scalars->SetValue(i,source_scalars->GetValue(pointId));
-                        //else
-                        //    target_scalars->SetValue(i,-1);
-		}
-	}
-	else
-	{
-		std::cout<<"Copying celldata"<<std::endl;
+            double closestpoint[3];
+            vtkIdType cellid;
+            int subid;
+            double dist2;
 
-		vtkShortArray *target_scalars = (vtkShortArray*) targetPd -> GetCellData() -> GetArray(property_name);
-		vtkShortArray *source_scalars = (vtkShortArray*) sourcePd -> GetCellData() -> GetArray(property_name);
+            vtkIdType pointId;
+            in_source_locator->FindClosestPoint(pt, closestpoint, cellid, subid, dist2);
 
-		vtkSmartPointer<vtkCellLocator> in_source_locator = vtkSmartPointer<vtkCellLocator>::New();
-		in_source_locator->SetDataSet(sourcePd);
-		in_source_locator->BuildLocator();
+            if (radius >= 0)
+                if (dist2 > radius * radius)
+                    cellid = -1;
 
-		for( int i=0; i<targetPd->GetNumberOfCells(); i++)
-		{
-			double cell_center_p[3];
-			double pt[3];
-			double weights[3];
-			int not_used;
-			targetPd->GetCell(i)->GetParametricCenter(cell_center_p);
-			targetPd->GetCell(i)->EvaluateLocation(not_used,cell_center_p,pt,weights);
+            if (cellid >= 0)
+                target_scalars->SetValue(i, source_scalars->GetValue(cellid));
+            //else
+            //    target_scalars->SetValue(i,-1);
+        }
+        std::cout<<std::endl;
+    }
 
-			double closestpoint[3];
-			vtkIdType cellid;
-			int subid;
-			double dist2;
+    char outputFileName[255];
+    strcpy(outputFileName, argv[4]);
+    std::cout << " save output to " << outputFileName << std::endl;
 
-                        vtkIdType pointId;
-                        in_source_locator->FindClosestPoint(pt,closestpoint,cellid,subid, dist2);
-                        
-                        if(radius>=0)
-                            if(dist2>radius*radius)
-                                cellid = -1;
-
-                        if(cellid>=0)
-                            target_scalars->SetValue(i,source_scalars->GetValue(cellid));
-                        //else
-                        //    target_scalars->SetValue(i,-1);
-		}
-	}
-
-	char outputFileName[255];
-	strcpy(outputFileName,argv[4]);
-	std::cout << " save output to " << outputFileName << std::endl;
-
-	CommonTools::SaveShapeToFile(targetPd,outputFileName);
+    CommonTools::SaveShapeToFile(targetPd, outputFileName);
 
 }
 
+int main(int argc, char *argv[]) {
 
+    if (argc < 5) {
+        std::cout << "Same as PassScalars, except that the search is done for every point/cell of target, not source!" << std::endl;
+        std::cout << "Usage: " << std::endl;
+        std::cout << argv[0] << " <source> <target> <property name> <output> <short|float> [radius]" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-int main( int argc, char *argv[] )
-{
-	
-	if( argc < 5 )
-	{
-		std::cout << "Same as PassScalars, except that the search is done for every point/cell of target, not source!" << std::endl;
-		std::cout << "Usage: " << std::endl;
-		std::cout << argv[0] << " <source> <target> <property name> <output> <short|float> [radius]" << std::endl;
-		return EXIT_FAILURE;
-	}
+    const char* datatype = argv[5];
 
-	const char* datatype = argv[5];
+    double radius = -1; //search radius
+    if (argv[6] != NULL)
+        radius = atof(argv[6]);
 
-        double radius = -1; //search radius
-        if (argv[6]!=NULL)
-            radius = atof(argv[6]);
-        
-	if( strcmp(datatype,"short")==0 )
-		PassScalarsShort( argc, argv, radius );
-	else if( strcmp(datatype,"float")==0 )
-		PassScalarsFloat( argc, argv, radius );
-	else
-	{
-		std::cout<<"Unknown data type: "<<datatype<<std::endl;
-		exit(-1);
-	}
+    if (strcmp(datatype, "short") == 0)
+        PassScalarsShort(argc, argv, radius);
+    else if (strcmp(datatype, "float") == 0)
+        PassScalarsFloat(argc, argv, radius);
+    else {
+        std::cout << "Unknown data type: " << datatype << std::endl;
+        exit(-1);
+    }
 
-	return 0;
+    return 0;
 
 }
