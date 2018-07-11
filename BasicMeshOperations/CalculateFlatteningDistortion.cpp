1e0cfd256358c88756b20c9985d540a751ceeac0
Constantine Butakoff <cbutakoff@gmail.com>
2018-07-11 22:05:22 +0200

    in progress

--- BasicMeshOperations/CalculateFlatteningDistortion.cpp
+++ BasicMeshOperations/CalculateFlatteningDistortion.cpp
@@ -16,39 +16,43 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkSmartPointer.h"
 #include "vtkCell.h"
 #include "VTKCommonTools.h"
+#include "Eigen/Dense"
 
-double TriangleGradientMatrix(arma::colvec2& q0, arma::colvec2& q1, arma::colvec2& q2, arma::mat& T,  bool normalize=true);
-double TriangleGradientMatrix(vtkPolyData* mesh, vtkIdType pt1id, vtkIdType pt2id, vtkIdType pt3id, arma::mat& T,  bool normalize=true);
-void FlattenTriangle(arma::colvec3& p0, arma::colvec3& p1, arma::colvec3& p2, arma::colvec2& q0, arma::colvec2& q1, arma::colvec2& q2);
+
+
+double TriangleGradientMatrix(Eigen::Vector2d& q0, Eigen::Vector2d& q1, Eigen::Vector2d& q2, Eigen::MatrixXd& T,  bool normalize=true);
+double TriangleGradientMatrix(vtkPolyData* mesh, vtkIdType pt1id, vtkIdType pt2id, vtkIdType pt3id, Eigen::MatrixXd& T,  bool normalize=true);
+void FlattenTriangle(Eigen::Vector3d& p0, Eigen::Vector3d& p1, Eigen::Vector3d& p2, Eigen::Vector2d& q0, Eigen::Vector2d& q1, Eigen::Vector2d& q2);
 
 
 
 int main( int argc, char *argv[] )
 {	
 
-	if( argc < 5 )
-	{
-		std::cout << "Usage: " << std::endl;
-		std::cout << argv[0] << " <original_mesh> <flattened_mesh> <output_flattened_mesh>" << std::endl;
-		std::cout << "flattened_mesh must have only x,y coordinates. Z coordinate is ignored" << std::endl;
-		return EXIT_FAILURE;
-	}
+    if( argc < 5 )
+    {
+            std::cout << "Usage: " << std::endl;
+            std::cout << argv[0] << " <original_mesh> <flattened_mesh> <output_flattened_mesh>" << std::endl;
+            std::cout << "flattened_mesh must have only x,y coordinates. Z coordinate is ignored" << std::endl;
+            return EXIT_FAILURE;
+    }
 
-    const char* mesh3d = argv[1]
-    const char* mesh2d = argv[2]
+    const char* mesh3d_filename = argv[1];
+    const char* mesh2d_filename = argv[2];
+    const char* output_filename = argv[3];
 
-	std::cout << " reading source " << argv[1] << std::endl;
-	vtkSmartPointer<vtkPolyData> sourcePd = vtkSmartPointer<vtkPolyData>::Take(
-		CommonTools::LoadShapeFromFile(argv[1]) );
+    std::cout << "reading original mesh " << mesh3d_filename << std::endl;
+    vtkSmartPointer<vtkPolyData> sourcePd = vtkSmartPointer<vtkPolyData>::Take(
+            CommonTools::LoadShapeFromFile(mesh3d_filename) );
 
-	std::cout << " reading target " << argv[2] << std::endl;
-	vtkSmartPointer<vtkPolyData> targetPd = vtkSmartPointer<vtkPolyData>::Take(
-		CommonTools::LoadShapeFromFile(argv[2]) );
+    std::cout << "reading flattened mesh " << mesh2d_filename << std::endl;
+    vtkSmartPointer<vtkPolyData> targetPd = vtkSmartPointer<vtkPolyData>::Take(
+            CommonTools::LoadShapeFromFile(mesh2d_filename) );
 
 
-	CommonTools::SaveShapeToFile(targetPd,outputFileName);
+    //CommonTools::SaveShapeToFile(targetPd,output_filename);
 
-	return 0;
+    return 0;
 }
 
 
@@ -64,9 +68,9 @@ int main( int argc, char *argv[] )
  * 
  *  @return W 2x3 matrix, the return value is twice the triangle area. 
  */
-double TriangleGradientMatrix(arma::colvec2& q0, arma::colvec2& q1, arma::colvec2& q2, arma::mat& T, bool normalize)
+double TriangleGradientMatrix(Eigen::Vector2d& q0, Eigen::Vector2d& q1, Eigen::Vector2d& q2, Eigen::MatrixXd& T,  bool normalize)
 {
-    T.set_size(2,3);
+    T.resize(2,3);
     T(0,0) = q1(1) - q2(1);
     T(0,1) = q2(1) - q0(1);
     T(0,2) = q0(1) - q1(1);
@@ -95,19 +99,19 @@ double TriangleGradientMatrix(arma::colvec2& q0, arma::colvec2& q1, arma::colvec
  * 
  *  @return W 2x3 matrix, the return value is twice the triangle area. 
  */
-double TriangleGradientMatrix(vtkPolyData* mesh, vtkIdType pt1id, vtkIdType pt2id, vtkIdType pt3id, arma::mat& T,  bool normalize)
+double TriangleGradientMatrix(vtkPolyData* mesh, vtkIdType pt1id, vtkIdType pt2id, vtkIdType pt3id, Eigen::MatrixXd& T,  bool normalize)
 {
-    arma::colvec3 pt1_R3;
-    arma::colvec3 pt2_R3;
-    arma::colvec3 pt3_R3;
+    Eigen::Vector3d pt1_R3;
+    Eigen::Vector3d pt2_R3;
+    Eigen::Vector3d pt3_R3;
 
-    mesh->GetPoint(pt1id, pt1_R3.colptr(0));
-    mesh->GetPoint(pt2id, pt2_R3.colptr(0));
-    mesh->GetPoint(pt3id, pt3_R3.colptr(0));
+    mesh->GetPoint(pt1id, pt1_R3.data());
+    mesh->GetPoint(pt2id, pt2_R3.data());
+    mesh->GetPoint(pt3id, pt3_R3.data());
 
     //project the points to the local coordinate system
     //define the axes
-    arma::colvec2 pt1_R2, pt2_R2, pt3_R2;
+    Eigen::Vector2d pt1_R2, pt2_R2, pt3_R2;
     FlattenTriangle(pt1_R3, pt2_R3, pt3_R3, pt1_R2, pt2_R2, pt3_R2);
 
     return TriangleGradientMatrix(pt1_R2, pt2_R2, pt3_R2, T, normalize);   
@@ -120,15 +124,15 @@ double TriangleGradientMatrix(vtkPolyData* mesh, vtkIdType pt1id, vtkIdType pt2i
  * 
  *  @return q0, q1, q2
  */
-void FlattenTriangle(arma::colvec3& p0, arma::colvec3& p1, arma::colvec3& p2, arma::colvec2& q0, arma::colvec2& q1, arma::colvec2& q2)
+void FlattenTriangle(Eigen::Vector3d& p0, Eigen::Vector3d& p1, Eigen::Vector3d& p2, Eigen::Vector2d& q0, Eigen::Vector2d& q1, Eigen::Vector2d& q2)
 {
     //project the points to the local coordinate system
     //define the axes
-    arma::colvec3 v0 = p2 - p1;
-    double pt12_dist = arma::norm(v0, 2);
-    v0 = arma::normalise(v0, 2);
-    arma::colvec3 v1 = arma::cross( arma::cross(v0, p2-p0), v0 );
-    v1 = arma::normalise(v1, 2);
+    Eigen::Vector3d v0 = p2 - p1;
+    double pt12_dist = v0.norm();
+    v0.normalize();
+    Eigen::Vector3d  v1 = (v0.cross(p2-p0)).cross (v0);
+    v1.normalize();
 
     //in local coordinates v0, v1
     //pt1 will have coordinates (0,0)
@@ -139,6 +143,6 @@ void FlattenTriangle(arma::colvec3& p0, arma::colvec3& p1, arma::colvec3& p2, ar
     
     q1(0) = pt12_dist;
 
-    q2(0) = arma::dot(v0, p2 - p0);
-    q2(1) = arma::dot(v1, p2 - p0);    
+    q2(0) = v0.dot(p2 - p0);
+    q2(1) = v1.dot(p2 - p0);    
 }
