4b9db38f83e51cd0ff43860616f177b0fdb386ba
Constantine Butakoff <cbutakoff@gmail.com>
2018-07-31 17:46:32 +0200

    added binary reader

--- BasicMeshOperations/ensight2vtk.cpp
+++ BasicMeshOperations/ensight2vtk.cpp
@@ -12,11 +12,13 @@ PURPOSE.  See the above copyright notice for more information.
 
 #include <vtkSmartPointer.h>
 #include <vtkEnSightGoldReader.h>
+#include <vtkEnSightGoldBinaryReader.h>
 #include <vtkMultiBlockDataSet.h>
 #include <vtkFieldData.h>
 #include <vtkDataArrayCollection.h>
 #include <vtkDataArray.h>
 #include <vtkDataSetWriter.h>
+#include <vtkEnSightReader.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -31,21 +33,31 @@ int main(int argc, char** argv)
         std::cout<<"Options: "<<std::endl;
         std::cout<<"-case <filename> -- filename of the ensight casefile"<<std::endl;
         std::cout<<"-o <prefix> -- prefix for the output filenames (will add numbers)"<<std::endl;
+        std::cout<<"-t [ASCII|ASCII] -- binary or ascii"<<std::endl;
         return -1;
     }
     
     std::string filename;
     std::string prefix = "data_";
+    bool isbinary = false;
     
     for (int c = 1; c < argc; c++) {
         if (strcmp(argv[c], "-case") == 0) 
             filename.assign( argv[++c] );
         else if (strcmp(argv[c], "-o") == 0) 
             prefix.assign( argv[++c] );
+        else if (strcmp(argv[c], "-t") == 0) 
+            if (strcmp(argv[++c], "BIN") == 0) 
+                isbinary = true;
     }    
     
         
-    vtkSmartPointer<vtkEnSightGoldReader> rdr = vtkSmartPointer<vtkEnSightGoldReader>::New();
+    vtkSmartPointer<vtkEnSightReader> rdr;
+    
+    if (!isbinary)
+        rdr = vtkSmartPointer<vtkEnSightGoldReader>::New();
+    else
+        rdr = vtkSmartPointer<vtkEnSightGoldBinaryReader>::New();
     
     //const char* filename = "/home/costa/Downloads/lvsmooth-nastin_normals/lvsmooth-nastin.ensi.case";
     rdr->SetCaseFileName(filename.c_str());
@@ -57,11 +69,11 @@ int main(int argc, char** argv)
         std::cout<<"Invalid file: "<<filename<<std::endl;        
     }
 
-    std::cout<<"Format type:"<<rdr->DetermineEnSightVersion()<<std::endl;
-    if (!rdr->DetermineEnSightVersion()!=vtkEnSightGoldReader::ENSIGHT_GOLD)
-    {
-        std::cout<<"Not insight gold ASCII format"<<filename<<std::endl;        
-    }
+    //std::cout<<"Format type:"<<rdr->DetermineEnSightVersion()<<std::endl;
+    //if (!rdr->DetermineEnSightVersion()!=vtkEnSightGoldReader::ENSIGHT_GOLD)
+    //{
+    //    std::cout<<"Not insight gold ASCII format"<<filename<<std::endl;        
+    //}
 
     
     rdr->DebugOff();
