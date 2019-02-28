36d5e84bb61e8fc0dd3adedcaec7cd07d42f1623
Constantine Butakoff <cbutakoff@gmail.com>
2019-02-28 17:39:02 +0100

    improved reader

--- Python/Alya/save_particles_pvd_bin.py
+++ Python/Alya/save_particles_pvd_bin.py
@@ -8,58 +8,32 @@ from os.path import isfile
 main_filename = 'fluidda.pvd'
 pts_filename = '../fluidda.pts.res'
 ncpus = 10
-nlastlines = 500000*50
 
 print('Parsing ',pts_filename)
 
-T =[]
-ILAGR =[]
-ITYPE = []
-EXIST = []
-COORX = []
-COORY = []
-COORZ = []
+
+dt = np.dtype([('T', 'f8'), ('ILAGR', 'i8'), ('ITYPE', 'i8'), ('EXIST', 'i8'),\
+ ('COORX', 'f8'), ('COORY', 'f8'), ('COORZ', 'f8'),\
+ ('VELOX', 'f8'), ('VELOY', 'f8'), ('VELOZ', 'f8'),\
+ ('DTK', 'f8'), ('CD', 'f8') ])
+
 with open(pts_filename, mode='rb') as file: # b is important -> binary
-    #get filesize
-    file.seek(0,2)
-    filesize = file.tell()
-    file.seek(0,0)
-
     hdr = file.read(255)
-    print(hdr)
+    data = np.fromfile(file, dtype=dt)
 
-    file.seek(filesize-nlastlines*12*8,0)
+df = pd.DataFrame.from_records(data)
 
 
-    with progressbar.ProgressBar(max_value=filesize) as bar:
-        while True:
-            T1 = np.fromfile(file, dtype=np.float64, count=1)
-            if T1.shape[0]==0: break #end of file
-            T.append(T1[0])
-
-            ILAGR.append( np.fromfile(file, dtype=np.int64, count=1)[0] )
-            ITYPE.append( np.fromfile(file, dtype=np.int64, count=1)[0] )
-            EXIST.append( np.fromfile(file, dtype=np.int64, count=1)[0] )
-            COORX.append( np.fromfile(file, dtype=np.float64, count=1)[0] )
-            COORY.append( np.fromfile(file, dtype=np.float64, count=1)[0] )
-            COORZ.append( np.fromfile(file, dtype=np.float64, count=1)[0] )
-            VELOX = np.fromfile(file, dtype=np.float64, count=1)
-            VELOY = np.fromfile(file, dtype=np.float64, count=1)
-            VELOZ = np.fromfile(file, dtype=np.float64, count=1)
-            DTK = np.fromfile(file, dtype=np.float64, count=1)
-            CD = np.fromfile(file, dtype=np.float64, count=1)
-
-            bar.update( file.tell() )
-
+#print(df)
 #
 #df = pd.read_csv(pts_filename, delim_whitespace=True,\
 # names=['T', 'ILAGR', 'ITYPE', 'EXIST', 'COORX', 'COORY', 'COORZ', 'VELOX', 'VELOY', 'VELOZ', 'DTK', 'CD'],\
 # header=0, comment='#')
-df = pd.DataFrame({'T':T,'ILAGR':ILAGR, 'ITYPE':ITYPE, 'EXIST':EXIST, 'COORX':COORX, 'COORY':COORY, 'COORZ':COORZ})
+#df = pd.DataFrame({'T':T,'ILAGR':ILAGR, 'ITYPE':ITYPE, 'EXIST':EXIST, 'COORX':COORX, 'COORY':COORY, 'COORZ':COORZ})
 
 df['T']=  df['T'].apply(lambda x: np.round(x,10))
 
-#print(df)
+#print(df['T'])
 
 #unique_times = df['T'].unique()
 unique_times = list(np.round(np.arange(0,100+(5e-2),5e-2),10))
@@ -77,6 +51,7 @@ def save_one_file(i):
     y = df1['COORY']
     z = df1['COORZ']
     tp = df1['ITYPE']
+    il = df1['ILAGR']
     pts.SetNumberOfPoints(df1.shape[0])
 
     if df1.shape[0]!=0:
@@ -87,13 +62,21 @@ def save_one_file(i):
         types.SetNumberOfComponents(1)
         types.SetNumberOfTuples(pts.GetNumberOfPoints())
         types.SetName('Type')
-        
+      
+        ilagr = vtk.vtkIntArray()
+        ilagr.SetNumberOfComponents(1)
+        ilagr.SetNumberOfTuples(pts.GetNumberOfPoints())
+        ilagr.SetName('ILAGR')
+
+
         for j in range(df1.shape[0]):         
             types.SetTuple1(j, tp.iloc[j])
+            ilagr.SetTuple1(j, il.iloc[j])
 
         pd = vtk.vtkPolyData()
         pd.SetPoints(pts)
         pd.GetPointData().AddArray(types)
+        pd.GetPointData().AddArray(ilagr)
 
         wr= vtk.vtkXMLPolyDataWriter()
         wr.SetInputData(pd)
