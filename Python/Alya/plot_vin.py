7e95b7e26875eccd7ad52433a4bd8264dc5bd687
Constantine <cbutakoff@gmail.com>
2020-04-27 23:16:42 +0200

    revamped and added more stuff

--- Python/Alya/plot_vin.py
+++ Python/Alya/plot_vin.py
@@ -1,44 +1,81 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
-print('Parameters: inputfile.exm.vin L1|L2|L3 [image_filename.ext]')
-print('Optional: image_filename.ext -- ext = png, jpg, svg,...')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("vin_file", metavar="problem.exm.vin", help='File with volume integrals')
+parser.add_argument("lead", metavar="L1|L2|L3", choices=['L1', 'L2', 'L3'], help='Lead to plot')
+parser.add_argument("-ca","--calcium", action='store_true', required=False, help='Add to plot calcium')
+parser.add_argument("-cl","--cyclelength", help='Cycle length in seconds', type=float, default = -1, required=False)
+parser.add_argument("-c","--cycles", metavar="N", nargs='*', type=int, required=False, help='Cycle numbers to plot from 0. Default all')
+parser.add_argument("-i","--image", metavar="image.png", required=False, help='Filename to save image (pg, jpg, svg,...)', default=None)
+args = parser.parse_args()
 
 
 columns = ['Iter','Time','rubbish','Ca','LL','RA','LA' ]
 
-lead2plot = sys.argv[2]
-saveimage = ''
-if len(sys.argv)>3:
-    saveimage = sys.argv[3]
 
-df = pd.read_csv(sys.argv[1], header=None, delim_whitespace=True, names=columns, comment='#')
+df = pd.read_csv(args.vin_file, header=None, delim_whitespace=True, comment='#')
+cols = list(df.columns)
+cols[0:2] = ['Iter', 'Time']
+cols[-4:] = ['Ca','LL','RA','LA']
 
-print(df.head())
+df.columns = cols
+
 df['L1'] = df['LA']-df['RA']
 df['L2'] = df['LL']-df['RA']
 df['L3'] = df['LL']-df['LA']
 
-fig, ax = plt.subplots()
+if args.cycles is None:
 
-ax.plot( df['Time'],df[lead2plot],'r',label=lead2plot)
-#ax.plot( df['Time'],df['L2'],'g',label='L2')
-#ax.plot( df['Time'],df['L3'],'b',label='L3')
-ax.set_ylabel('Voltage(mV)')
-ax.set_xlabel('Time(s)')
 
-ax2 = ax.twinx()
-ax2.plot(df['Time'],df['Ca'],'b',label='Ca')
-ax2.plot([], [], 'r', label = lead2plot)
-ax2.set_ylabel('Calcium transient (umol)')
+    fig, ax = plt.subplots()
+
+    ax.plot( df['Time'],df[args.lead],'r',label=args.lead)
+    ax.set_ylabel('Voltage(mV)')
+    ax.set_xlabel('Time(s)')
+
+    if args.calcium:
+        ax2 = ax.twinx()
+        ax2.plot(df['Time'],df['Ca'],'b',label='Ca')
+        ax2.plot([], [], 'r', label = args.lead)
+        ax2.set_ylabel('Calcium transient (umol)')
+
+else:
+    df1 = pd.DataFrame()
+    df1["Time"] = df[df['Time']<=args.cyclelength]['Time']
+
+    if args.calcium:
+        for i in args.cycles:
+            ta = np.round(i*args.cyclelength,10)
+            tb = np.round((i+1)*args.cyclelength,10)
+            dd = df[(df['Time']>ta) & (df['Time']<=tb)]['Ca'].values
+            df1[f'Ca{i}']=np.NaN
+            df1[f'Ca{i}'].iloc[0:dd.shape[0]]=dd
+
+        #print(df1.head())
+        df1.plot(x='Time')
+        plt.ylabel('Calcium transient (umol)')
+    else:
+        for i in args.cycles:
+            ta = np.round(i*args.cyclelength,10)
+            tb = np.round((i+1)*args.cyclelength,10)
+            dd = df[(df['Time']>ta) & (df['Time']<=tb)][args.lead].values
+            #print(df[(df['Time']>i*args.cyclelength) & (df['Time']<=(i+1)*args.cyclelength)]['Time'])
+            df1[f'{args.lead}{i}']=np.NaN
+            df1[f'{args.lead}{i}'].iloc[0:dd.shape[0]]=dd
+
+        df1.plot(x='Time')
+        plt.ylabel('Voltage(mV)')
+  
 
 plt.legend()
 
 
-if len(saveimage)>0:
-    plt.savefig(saveimage)
+if not args.image is None:
+    plt.savefig(args.image)
 else:
     plt.show()
 
