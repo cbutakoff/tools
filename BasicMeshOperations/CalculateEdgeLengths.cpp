528f28ad7d0906234b9c7462ac5a5bc6125b519a
Constantine <cbutakoff@elem.bio>
2023-01-31 14:32:07 +0100

    changed the code to produce min/max edge length and save with the mesh as a quality metric

--- BasicMeshOperations/CalculateEdgeLengths.cpp
+++ BasicMeshOperations/CalculateEdgeLengths.cpp
@@ -6,15 +6,20 @@ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notice for more information.
 =========================================================================*/
 
-/*! \file 
-    \brief adds scalar array ro polydata and sets its value to a constant
+/*! \file
+		\brief adds scalar array ro polydata and sets its value to a constant
 */
 #include <vtkDataSetReader.h>
+#include <vtkXMLUnstructuredGridReader.h>
+#include <vtkXMLUnstructuredGridWriter.h>
+#include <vtkUnstructuredGrid.h>
 #include <vtkDataSet.h>
 #include <vtkCell.h>
 #include <vtkExtractEdges.h>
+#include <vtkNew.h>
+#include <vtkFloatArray.h>
+#include <vtkCellData.h>
 #include "VTKCommonTools.h"
-
 
 #include <vector>
 #include <iostream>
@@ -24,146 +29,141 @@ PURPOSE.  See the above copyright notice for more information.
 #include <limits>
 
 #ifdef _OPENMP
-# include <omp.h>
+#include <omp.h>
 #endif
-
 
 using namespace std;
 
-
-
-
-
-
 //------------------------------------------------------------------
-int main( int argc, char *argv[] )
-{	
+int main(int argc, char *argv[])
+{
 
-	if( argc <2  )
+	if (argc < 2)
 	{
-		cout<<"Saves max edge length per cell into a text file"<<endl;
-		cout<<"Supports openmp"<<endl<<endl;
+		cout << "Saves max edge length per cell into a text file" << endl;
+		cout << "Supports openmp" << endl
+				 << endl;
 		std::cout << "Usage: " << std::endl;
-		std::cout << argv[0] << "SetScalars <mesh.vtk> <outputfile.txt>" << std::endl;
+		std::cout << argv[0] << "SetScalars <mesh.vtk|vtu> <outputfile.vtu>" << std::endl;
 		return EXIT_FAILURE;
 	}
 
-
-	int c=1;
+	int c = 1;
 	const char *filenamein = argv[c++];
 	const char *filenameout = argv[c++];
 
-
 	int ncpus = 1;
-	#ifdef _OPENMP
+#ifdef _OPENMP
 	ncpus = omp_get_max_threads();
-	cout<<"OpenMP enabled, using "<<ncpus<<" threads"<<endl<<endl;
-	#endif
-	
+	cout << "OpenMP enabled, using " << ncpus << " threads" << endl
+			 << endl;
+#endif
+
+	vtkSmartPointer<vtkUnstructuredGrid> mesh;
 
 	cout << " opening " << filenamein << endl;
-	vtkSmartPointer<vtkDataSetReader> rd = vtkSmartPointer<vtkDataSetReader>::New();
-	rd->SetFileName(filenamein);
-	CommonTools::AssociateProgressFunction(rd);
-  	rd->Update();
+	if (filenamein[strlen(filenamein) - 1] == 'u' || filenamein[strlen(filenamein) - 1] == 'U')
+	{ //.vtu
+		vtkNew<vtkXMLUnstructuredGridReader> rd;
+		rd->SetFileName(filenamein);
+		CommonTools::AssociateProgressFunction(rd);
+		rd->Update();
+		mesh = vtkUnstructuredGrid::SafeDownCast(rd->GetOutput());
+	}
+	else
+	{
+		vtkNew<vtkDataSetReader> rd;
+		rd->SetFileName(filenamein);
+		CommonTools::AssociateProgressFunction(rd);
+		rd->Update();
+		mesh = vtkUnstructuredGrid::SafeDownCast(rd->GetOutput());
+	}
 
-	auto mesh = rd->GetOutput();
-
-	vector<double> edgelength_global;
-	edgelength_global.reserve(rd->GetOutput()->GetNumberOfCells());
-
+	vtkNew<vtkFloatArray> els;
+	els->SetNumberOfComponents(2);
+	els->SetNumberOfTuples(mesh->GetNumberOfCells());
+	els->SetName("EdgeLengthMinMax");
 
 	vtkIdType steps_completed = 0;
-	const vtkIdType step_size   = 100;
-	const vtkIdType total_steps = rd->GetOutput()->GetNumberOfCells() / step_size + 1;
+	const vtkIdType step_size = 100;
+	const vtkIdType total_steps = mesh->GetNumberOfCells() / step_size + 1;
 
-	#pragma parallel 
+#pragma parallel
 	{
-	vector<double> edgelength_local;
-	edgelength_local.reserve( round( rd->GetOutput()->GetNumberOfCells()/ncpus ) );
+		vector<double> edgelength_max_local;
+		vector<double> edgelength_min_local;
+		vector<vtkIdType> cellIds_local;
+		edgelength_max_local.reserve(round(mesh->GetNumberOfCells() / ncpus));
+		edgelength_min_local.reserve(round(mesh->GetNumberOfCells() / ncpus));
+		cellIds_local       .reserve(round(mesh->GetNumberOfCells() / ncpus));
 
-	vtkIdType local_steps_count = 0;
+		vtkIdType local_steps_count = 0;
 
-
-	#pragma omp for nowait schedule(auto) 
-	for( vtkIdType i=0; i<rd->GetOutput()->GetNumberOfCells(); i++ )
-	{
-            	if (local_steps_count++ % step_size == step_size-1)
-            	{
-			#pragma omp atomic
-                	++steps_completed;
-
-	                if (steps_completed % 100 == 1)
-                	{
-				#pragma omp critical
-				cout<<"Done "<<steps_completed<<"/"<<total_steps<<"; "<< steps_completed*100/total_steps<< "%\r"<<flush;
-                	}
-            	}
-
-		auto cell = mesh->GetCell(i);
-		if(  cell->GetNumberOfPoints()!=4 )
+#pragma omp for nowait schedule(auto)
+		for (vtkIdType i = 0; i < mesh->GetNumberOfCells(); i++)
 		{
-			cout<<endl<<"Only tetras supported. Cell "<< i <<" has "<<cell->GetNumberOfPoints()<<" vertices"<<endl;
-			throw;
-		}
-
-		double coordinates[4][3];
-		
-		for(int kkk=0; kkk<4; kkk++)
-			mesh->GetPoint( cell->GetPointId(kkk), coordinates[kkk] );
-		
-
-		int edge_ids[6][2] =
-			    {
-				{1,0},
-				{2,1},
-				{0,2},
-				{3,0},
-				{3,1},
-				{3,2}
-			    };
-
-		double edge[3];
-		double max_dist = 0;
-
-		for( int kkk=0; kkk<6; kkk++)
-		{
-			for(int xyz=0; xyz<3; xyz++)
+			if (local_steps_count++ % step_size == step_size - 1)
 			{
-				edge[xyz] = coordinates[ edge_ids[kkk][0] ][xyz] - coordinates[ edge_ids[kkk][1] ][xyz];
+#pragma omp atomic
+				++steps_completed;
+
+				if (steps_completed % 100 == 1)
+				{
+#pragma omp critical
+					cout << "Done " << steps_completed << "/" << total_steps << "; " << steps_completed * 100 / total_steps << "%\r" << flush;
+				}
 			}
-			const double d = sqrt( edge[0]*edge[0] + edge[1]*edge[1] + edge[2]*edge[2] );
 
-			if( d > max_dist ) max_dist = d;
+			auto cell = mesh->GetCell(i);
+		  vtkIdType nEdges = cell->GetNumberOfEdges();
+
+			double max_dist = 0;
+			double min_dist = std::numeric_limits<double>::max();
+
+			for( vtkIdType edgeid=0; edgeid<nEdges; edgeid++){
+				vtkCell* edge = cell->GetEdge(edgeid);
+				const vtkIdType pt0id = edge->GetPointId(0);
+				const vtkIdType pt1id = edge->GetPointId(1);
+				double pt0[3], pt1[3];
+				mesh->GetPoint(pt0id, pt0);
+				mesh->GetPoint(pt1id, pt1);
+				const double d = sqrt( (pt1[0]-pt0[0])*(pt1[0]-pt0[0]) + 
+      				 								 (pt1[1]-pt0[1])*(pt1[1]-pt0[1]) + 
+      												 (pt1[2]-pt0[2])*(pt1[2]-pt0[2]) );
+      
+				if (d > max_dist)
+					max_dist = d;
+				if (d < min_dist)
+					min_dist = d;
+
+			}
+
+			edgelength_max_local.push_back(max_dist);
+			edgelength_min_local.push_back(min_dist);
+			cellIds_local.push_back(i);
 		}
+		// end parallel for
+		cout << endl;
 
-		edgelength_local.push_back(max_dist);
+#pragma omp critical
+		{
+			for( size_t i=0; i<cellIds_local.size(); i++ )
+			{
+    		els->SetTuple2( cellIds_local[i], edgelength_min_local[i], edgelength_max_local[i] );
+			}
+		} // end of critical
 
-  	}
-	//end parallel for
-  	cout<<endl;
-	
+	} // end parallel
 
-	#pragma omp critical
-	{
-	#ifdef _OPENMP
-	//cout<<edgelength_local[0]<<endl;
-	copy(edgelength_local.begin(), edgelength_local.end(), back_inserter(edgelength_global));
-	#else
-	edgelength_global.assign( edgelength_local.begin(), edgelength_local.end() );
-	#endif
-	}// end of critical
+	cout << "Saving the edge lengths mesh" << endl;
 
-	}//end parallel
+	mesh->GetCellData()->AddArray(els);
 
-	cout<<"Saving the edge lengths"<<endl;
-
-
-	ofstream ff( filenameout );
-	for (vtkIdType k=0; k<edgelength_global.size(); ++k)
-	{
-		ff<<edgelength_global[k]<<endl;
-	}
+	vtkNew<vtkXMLUnstructuredGridWriter> wr;
+	wr->SetFileName(filenameout);
+	wr->SetInputData(mesh);
+	wr->EncodeAppendedDataOff();
+	wr->Write();
 
 
 	return EXIT_SUCCESS;
